56bfb0441a2db11b35b034312ee3d989a4cef6d1
PatelShakil <patelsakib95@gmail.com>
2024-05-04 15:44:16 +0530

    Add project files.

---
+++ Controllers/Users.cs
@@ -0,0 +1,65 @@
+﻿using AspApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AspApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public Users(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public string GetAllUsers()
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("AspApiCon").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("Select * from users_master", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            var list = new List<UserModel>();
+            if(dt.Rows.Count > 0)
+            {
+                return JsonConvert.SerializeObject(dt.Rows[0].Table);
+            }
+            else
+            {
+                var res = new Response();
+                res.status = 404;
+                res.message = "No Data Found";
+                return JsonConvert.SerializeObject(res);
+            }
+
+            for (var i = 0;i < dt.Rows.Count;i++)
+            {
+                var user = new UserModel();
+                //user.Id = Convert.ToInt32(dt.Rows[i]["Id"]);
+                //user.Name = Convert.ToString(dt.Rows[i]["Name"]);
+                //user.Email = Convert.ToString(dt.Rows[i]["Email"]);
+                //list.Add(user);
+                
+            }
+            if(list.Count > 0)
+            {
+                //return JsonConvert.SerializeObject(list);
+            }
+            else
+            {
+                var res = new Response();
+                res.status = 404;
+                res.message = "No Data Found";
+                //return JsonConvert.SerializeObject(res);
+            }
+        }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace AspApi.Models
+{
+    public class Response
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+    }
+}
